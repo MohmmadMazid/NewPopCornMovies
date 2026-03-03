b481925f4c51918eb229cbce41040e1dfaeaac0c
MohmmadMazid <mazidmansury5500@gmail.com>
2026-03-03 13:25:50 +0530

    start rating component made

--- src/StarRattingComponent/StarRating.jsx
+++ src/StarRattingComponent/StarRating.jsx
@@ -1,32 +1,65 @@
 import React, { useState } from 'react'
 import Stars from './Stars'
+import PropTypes from "prop-types"
 
 const containerStyle = {
-    display:"flex",
-    alignItems:"center",
-    gap:"16px"
+    display: "flex",
+    alignItems: "center",
+    gap: "16px"
 }
 
 const starContainerStyle = {
-    display:"flex",
-    gap:"4px"
+    display: "flex",
+    gap: "4px"
 }
 
-const StarRating = ({length=5}) => {
-    const [rating,setRating]=useState(1)
 
-    const handleStarRating = (rating)=>{
+
+const StarRating = ({ length = 5,message=[] ,color="#fcc419",size=48, className="",setMovieRating}) => {
+    const [rating, setRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
+
+    const textStyle = {
+    lineHeight:"1px",
+    margin:0,
+    color,
+    fontSize:`${size/1.5}px`
+}
+
+    const handleStarRating = (rating) => {
         setRating(rating)
+        setMovieRating(rating)
     }
-  return (
-    <div style={containerStyle}>
-        <div style={starContainerStyle}>
 
-        {Array.from({length:length},(_,i)=>(<Stars key={i}  onRate={()=>handleStarRating(i+1)} full={rating>=i+1} />))}
+    const handleHoverIn= (tempRating)=>{
+        setTempRating(tempRating)
+    }
+
+    const handleHoverOut = (tempRating)=>{
+        setTempRating(tempRating)
+    }
+    return (
+        <div style={containerStyle}>
+            <div style={starContainerStyle}>
+
+                {/* {Array.from({ length: length }, (_, i) => (<Stars key={i} onRate={() => handleStarRating(i + 1)} full={rating >= i + 1} onHoverIn={()=>setTempRating(i + 1)} onHoverOut={()=>setTempRating(0)} />))} */}
+                {Array.from({length:length},(_,i)=>(<Stars key={i}  onRate={()=>handleStarRating(i+1)} full={tempRating?tempRating>=i+1:rating>=i+1} 
+                 onHoverIn = {()=>handleHoverIn(i+1)} 
+                 onHoverOut={()=>handleHoverOut(0)} color={color} size={size} />))}
+            </div>
+
+            <p style={textStyle}> {message.length==length?message[tempRating?tempRating-1:rating-1] : tempRating||rating || ""}</p>
+                       {/* <p style={textStyle}> {message.length>0?message[tempRating?tempRating-1:rating-1] : tempRating||rating || ""}</p> */}
         </div>
-        <p className='textStyle'> {rating || ""}</p>
-    </div>
-  )
+    )
 }
 
+StarRating.propTypes = {
+  length: PropTypes.number,
+  message: PropTypes.arrayOf(PropTypes.string),
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  setMovieRating:PropTypes.func,
+};
 export default StarRating
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import StarRating from './StarRattingComponent/StarRating.jsx'
+import MovieRating from './StarRattingComponent/MovieRating.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating  length={5} />
+    <StarRating  length={5} message={["terrible","bad","okay","good","amazing"]} />
+    <StarRating  size={30} color='green' className='test' />
+    <MovieRating  />
   </StrictMode>,
 )
